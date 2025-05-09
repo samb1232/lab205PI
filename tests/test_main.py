08a5450e0d8323533d20f8bc9f50af95d5f3bbc9
samb1232 <arterm212@gmail.com>
2025-05-09 17:20:02 +0500

    added tests and gitignore

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
 import pytest
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from main import multiply_by_ten
 
 def test_multiply_by_ten_int():
